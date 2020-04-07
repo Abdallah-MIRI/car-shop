49d9d738132f761a80af586080a99fe2fd41138a
Abdallah Mohamed <abdallahhaboharba@gmail.com>
2020-04-07 10:23:52 +0200

    Some Changes

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+/* 
+** Author: Abdallah Mohammed
+** Theme: Shop Buy
+** Version: 1.0
+*/
+
+
+
+
+// Slideshow
+const container = document.getElementById('containerpart');
+
+var part = document.getElementsByClassName('sell-part');
+
+// function slideShow() {
+//     for (var i = 0; i < part.length; i++) {
+
+//         part[i].classList.add('active');
+//         console.log(part);
+//     }
+// }
+
+// setTimeout(slideShow, 3000);
+
