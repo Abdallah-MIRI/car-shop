34512763041952fc3029cae1e2fa42613c63a7be
Abdallah-MIRI <abdallahhaboharba.com>
2020-05-09 07:12:28 +0200

    New File

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,95 +1,157 @@
-/* 
+/*
 ** Author: Abdallah Mohammed
-** Theme: Shop Buy
-** Version: 1.0
+** Theme: Badr City
+** Version: 1.0.0
 */
 
-
-// Scroll Header
-var headerShadow = document.getElementById('header-fixed');
-
-window.onscroll = function () {
-
-    if ( document.body.scrollTop > 80 || document.documentElement.scrollTop > 80 ) {
-        headerShadow.classList.add('shadow-sm');
-    } else {
-        headerShadow.classList.remove('shadow-sm');
-    }
-}
-
-
-// Icon Hamburger Menu
-var iconHam = document.getElementById('icon-hamburger'),
-    menuShow = document.getElementById('menu-show');
-
-iconHam.onclick = function () {
-    iconHam.classList.toggle('open');
-    menuShow.classList.toggle('active-menu');
-}
-
-
-
-
-// Slideshow
-const container = document.getElementById('containerpart');
-
-var part = document.getElementsByClassName('sell-part'),
-    prev = document.getElementById('prev'),
-    next = document.getElementById('next');
-
-// Width Device
-function widthDevice() {
-    if (screen.width ===  1024) {
-        for (var i = 0; i < container.children.length; i++) {
-            container[i] = 2;
-        }
-    }
-}
-widthDevice();
-
-// Prev
-/* prev.onclick = function () {
-
-    for (var i = 0; i < part.length; i++) {
-
-        container.firstElementChild.classList.remove('active');
-        container.lastElementChild.classList.add('active');
-
-    }
-}
-
-// Next
-next.onclick = function () {
-
-    for (var i = 0; i < part.length; i++) {
-
-        container.lastElementChild.classList.add('active');
-        container.firstElementChild.classList.remove('active');
-
-    }
-} */
-
-// function slideShow() {
-//     for (var i = 0; i < part.length; i++) {
-
-//         part[i].classList.add('active');
-//         console.log(part);
+/*-----------
+Scroll Header
+ -----------*/
+// var headerShadow = document.getElementById('header-fixed');
+//
+// window.onscroll = function () {
+//
+//     if ( document.body.scrollTop > 80 || document.documentElement.scrollTop > 80 ) {
+//         headerShadow.classList.add('shadow-sm');
+//     } else {
+//         headerShadow.classList.remove('shadow-sm');
 //     }
 // }
 
-// setTimeout(slideShow, 3000);
 
 
-/* 
-// Show and Hidden Filters Page Search
-*/
-// var btnShow = document.getElementById('showcat'),
-//     icon    = document.getElementById('iconfilter'),
-//     filters = document.getElementById('type-filters');
-
-// btnShow.onclick = function () {
-//     filters.classList.toggle('hidden-filters');
-//     btnShow.children.classList.toggle('hidden');
+/*-----------
+Icon Hamburger Menu
+ -----------*/
+// var iconHam = document.getElementById('icon-hamburger'),
+//     menuShow = document.getElementById('menu-show');
+//
+// iconHam.onclick = function () {
+//     iconHam.classList.toggle('open');
+//     menuShow.classList.toggle('active-menu');
 // }
 
-// console.log(btnShow.children);
+
+/*-----------
+Slideshow Head Site
+-----------*/
+ // Get Posts Slide
+ let postSlide = Array.from(document.getElementsByClassName('post-slide')),
+     // Slides Count
+     slidesCount = postSlide.length,
+     // Set Start Slide
+     startSlide = 0,
+     // Get Dots
+     dots = document.getElementById('dots');
+
+
+// Craet List Items Based on Slide Count
+for (let i = 1; i <= slidesCount; i++) {
+
+  // Creat Items Span
+  let creatSpan = document.createElement('span');
+
+  // Set Custom Attribute
+  creatSpan.setAttribute('data-index', i);
+
+  // Add Class List
+  creatSpan.setAttribute('class', 'items');
+
+  // Set Append Child Ul
+  dots.appendChild(creatSpan);
+}
+
+// Get Dots items
+let items = Array.from(document.getElementsByClassName('items'));
+
+// Loop Through All Bullets Items
+for (let d = 0; d < items.length; d++) {
+
+    items[d].onclick = function () {
+
+        startSlide = parseInt(this.getAttribute('data-index'));
+        theChecker();
+    }
+}
+
+// Auto Slide Posts
+function autoSlide() {
+
+  for (i = 0; i < slidesCount; i++) {
+    // Remove All Active
+    removeAllActive();
+  }
+
+  startSlide++;
+  if (startSlide > slidesCount) {
+    startSlide = 1;
+  }
+  // Set Active Class on Current Slide
+  postSlide[startSlide - 1].classList.add('active');
+
+  // Set Active class on Current Pagination Item
+  items[startSlide - 1].classList.add('active');
+  setTimeout(autoSlide, 3000);
+}
+
+window.onload = setTimeout(autoSlide, 2000);
+
+// Create The Checker Function
+function theChecker() {
+
+  // Remove All Active
+  removeAllActive();
+
+  // Set Active Class on Current Slide
+  postSlide[startSlide - 1].classList.add('active');
+
+  // Set Active class on Current Pagination Item
+  items[startSlide - 1].classList.add('active');
+}
+
+// Remove All Active Class From Images and Pagination Bullets
+function removeAllActive() {
+
+    // Loop Through Images
+    postSlide.forEach(function (post) {
+        post.classList.remove('active');
+    });
+
+    // Loop Through Pagination Bullets
+    items.forEach(function (bullet) {
+
+        bullet.classList.remove('active');
+
+    });
+
+}
+
+
+/*-----------
+Slideshow Items Name Category
+-----------*/
+// Get Button Previous
+let btnPrev = document.getElementById('btn-prev'),
+    // Get Button Next
+    btnNext = document.getElementById('btn-next'),
+    // Get Items
+    itemsNameCat = Array.from(document.getElementsByClassName('items-name-cat')),
+    // Items Count
+    itemsCount = itemsNameCat.length,
+    // Items Start
+    itemsStart = 0;
+
+console.log(btnPrev);
+console.log(btnNext);
+console.log(itemsNameCat);
+console.log(itemsCount);
+
+
+/*-----------
+Window Onload Map Display Block
+-----------*/
+let map = document.getElementById('map');
+
+window.onload = setTimeout(function () {
+  map.style.opacity = "1";
+}, 2000);
